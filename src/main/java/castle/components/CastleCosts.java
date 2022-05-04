5d2b2bacb45f7e35030de34b593c7cc2cda5542f
BustyBoi <104872092+BustyBoi@users.noreply.github.com>
2022-05-04 12:31:39 -0500

    Massive Balance Changes
    I might have messed up in the ore cost, idk how to fix cuz I'm on mobile

--- src/main/java/castle/components/CastleCosts.java
+++ src/main/java/castle/components/CastleCosts.java
@@ -19,79 +19,80 @@ public class CastleCosts {
 
     public static void load() {
         units = OrderedMap.of(
-                UnitTypes.dagger,   new Moneys(60,    0,  10),
+                UnitTypes.dagger,   new Moneys(50,    0,  10),
                 UnitTypes.mace,     new Moneys(150,   1,  50),
-                UnitTypes.fortress, new Moneys(500,   4,  150),
-                UnitTypes.scepter,  new Moneys(3000,  20, 750),
+                UnitTypes.fortress, new Moneys(650,   4,  200),
+                UnitTypes.scepter,  new Moneys(3000,  20, 800),
                 UnitTypes.reign,    new Moneys(10000, 45, 1500),
 
-                UnitTypes.crawler,  new Moneys(75,    0,  10),
-                UnitTypes.atrax,    new Moneys(160,   1,  60),
-                UnitTypes.spiroct,  new Moneys(500,   4,  150),
+                UnitTypes.crawler,  new Moneys(30,    0,  15),
+                UnitTypes.atrax,    new Moneys(180,   1,  60),
+                UnitTypes.spiroct,  new Moneys(700,   4,  150),
                 UnitTypes.arkyid,   new Moneys(4600,  24, 750),
                 UnitTypes.toxopid,  new Moneys(12500, 50, 1750),
 
-                UnitTypes.nova,     new Moneys(75,    0,  15),
-                UnitTypes.pulsar,   new Moneys(160,   1,  50),
-                UnitTypes.quasar,   new Moneys(500,   4,  175),
+                UnitTypes.nova,     new Moneys(70,    0,  15),
+                UnitTypes.pulsar,   new Moneys(180,   1,  50),
+                UnitTypes.quasar,   new Moneys(650,   4,  175),
                 UnitTypes.vela,     new Moneys(3750,  25, 750),
-                UnitTypes.corvus,   new Moneys(15000, 55, 1500),
+                UnitTypes.corvus,   new Moneys(13500, 60, 1500),
 
-                UnitTypes.risso,    new Moneys(150,   0,  25),
-                UnitTypes.minke,    new Moneys(350,   2,  75),
-                UnitTypes.bryde,    new Moneys(1200  ,6,  200),
-                UnitTypes.sei,      new Moneys(3750,  22, 800),
-                UnitTypes.omura,    new Moneys(15000, 50, 1750),
+                UnitTypes.risso,    new Moneys(200,   0,  50),
+                UnitTypes.minke,    new Moneys(400,   2,  75),
+                UnitTypes.bryde,    new Moneys(1000,  5,  250),
+                UnitTypes.sei,      new Moneys(3800,  24, 800),
+                UnitTypes.omura,    new Moneys(17500, 50, 1750),
 
                 UnitTypes.retusa,   new Moneys(160,   0,  25),
                 UnitTypes.oxynoe,   new Moneys(650,   3,  80),
                 UnitTypes.cyerce,   new Moneys(1300,  6,  200),
-                UnitTypes.aegires,  new Moneys(4800,  24, 800),
+                UnitTypes.aegires,  new Moneys(5300,  24, 1000),
                 UnitTypes.navanax,  new Moneys(11000, 70, 2000)
         );
 
         turrets = OrderedMap.of(
-                Blocks.duo,        100,
-                Blocks.scatter,    250,
-                Blocks.scorch,     200,
-                Blocks.hail,       450,
-                Blocks.wave,       300,
-                Blocks.lancer,     350,
-                Blocks.arc,        150,
-                Blocks.swarmer,    1250,
-                Blocks.salvo,      500,
-                Blocks.tsunami,    850,
-                Blocks.fuse,       1500,
-                Blocks.ripple,     1500,
-                Blocks.cyclone,    1750,
-                Blocks.foreshadow, 4000,
-                Blocks.spectre,    3000,
-                Blocks.meltdown,   3000
+                Blocks.duo,        50,
+                Blocks.scatter,    150,
+                Blocks.scorch,     100,
+                Blocks.hail,       150,
+                Blocks.wave,       250,
+                Blocks.lancer,     500,
+                Blocks.arc,        80,
+                Blocks.swarmer,    1850,
+                Blocks.salvo,      800,
+                Blocks.tsunami,    800,
+                Blocks.fuse,       1250,
+                Blocks.ripple,     1200,
+                Blocks.cyclone,    2650,
+                Blocks.foreshadow, 6000,
+                Blocks.spectre,    4000,
+                Blocks.meltdown,   3500
         );
 
         items = OrderedMap.of(
-                Items.copper,        500,
-                Items.lead,          500,
-                Items.metaglass,     750,
-                Items.graphite,      750,
-                Items.sand,          750,
-                Items.coal,          750,
+                Items.copper,        250,
+                Items.lead,          250,
+                Items.metaglass,     600,
+                Items.graphite,      400,
+                Items.sand,          50,
+        );
+                Items.coal,          50,
                 Items.titanium,      800,
-                Items.thorium,       1000,
-                Items.scrap,         500,
-                Items.silicon,       750,
-                Items.plastanium,    1250,
-                Items.phaseFabric,   2000,
-                Items.surgeAlloy,    2500,
+                Items.thorium,       1250,
+                Items.scrap,         50,
+                Items.silicon,       500,
+                Items.plastanium,    1500,
+                Items.phaseFabric,   1600,
+                Items.surgeAlloy,    1800,
                 Items.sporePod,      500,
                 Items.blastCompound, 750,
                 Items.pyratite,      750
         );
 
         effects = OrderedMap.of(
-                StatusEffects.overclock, 7500,
-                StatusEffects.overdrive, 15000,
-                StatusEffects.boss,      25000
+                StatusEffects.overclock, 5000,
+                StatusEffects.overdrive, 16500,
+                StatusEffects.boss,      28000
         );
     }
 
