29c3c4ddd74cf08a9e2ac714d594ab29466e85e8
RKN-bot-discord <autosubtitletest@gmail.com>
2026-01-14 22:57:06 +0300

    turn ai back to stupid cuz no one dont sure abt this change

--- src/main/java/castle/CastleCommandAI.java
+++ src/main/java/castle/CastleCommandAI.java
@@ -5,8 +5,6 @@ import mindustry.ai.types.CommandAI;
 import mindustry.gen.Teamc;
 import mindustry.world.blocks.defense.turrets.Turret;
 import mindustry.world.blocks.production.Drill;
-import mindustry.gen.Unit;
-import mindustry.entities.Units;
 
 import static castle.CastleUtils.*;
 import static mindustry.entities.Units.*;
@@ -17,26 +15,12 @@ public class CastleCommandAI extends CommandAI {
     public void updateUnit() {
         if (!hasCommand() && onEnemySide(unit)) {
              target = attackTarget = unit.closestEnemyCore();
-            if (unit.type.flying){
-                if (unit.health>unit.type.health*0.6) moveTo(target, unit.type.range * 0.8f);
-                else moveTo(target, 0f);
-            }
-            else pathfind(Pathfinder.fieldCore);
 
-            Unit enemy = Units.closestEnemy(
-                unit.team,
-                unit.x,
-                unit.y,
-                unit.type.range,
-                u -> u.checkTarget(unit.type.targetAir, unit.type.targetGround)
-            );
-            if (enemy != null) {
-                target = attackTarget = enemy;
-            }
+            if (unit.type.flying) moveTo(target, unit.type.range * 0.8f);
+            else pathfind(Pathfinder.fieldCore);
 
             if (!invalid(target) && unit.type.circleTarget)
                 circleAttack(80f);
-            
 
             updateWeapons();
             faceTarget();
